bb826a8bcbfdb82d93273c1930e37bb419ed3cee
wlhhlw <darian.wang@gymboglobal.com>
2019-09-05 18:19:34 +0800

    test

--- src/main/java/com/example/freemarkertest/controller.java
+++ src/main/java/com/example/freemarkertest/controller.java
@@ -32,7 +32,7 @@ public class controller {
     public ModelAndView testList(){
         ModelAndView mv = new ModelAndView("/index");
         List<User> userList = new ArrayList<>();
-        userList.add(new User("xiaoming","18","男"));
+        userList.add(new User("xiaoming ","18","男"));
         userList.add(new User("花花","20","nv"));
         mv.addObject("userList",userList);
         mv.addObject("userJson", JSONObject.toJSON(userList).toString());
@@ -46,7 +46,6 @@ public class controller {
 
     @GetMapping("/t")
     public String t(){
-
         return "/test";
     }
 }
